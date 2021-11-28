691e410ff7d67e1b161ff8e7f557c655fad7e141
aalvarezwindey <ajalvarez@fi.uba.ar>
2021-11-28 20:34:43 -0300

    Implement Logo v1 component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Logo = styled.img`
+  width: 250px;
+  border-radius: 50%;
+  border: 1px solid #00000044;
+  width: inherit;
+  height: inherit;
+  object-fit: contain;
+  padding: 0.5em;
+  box-sizing: border-box;
+`;
+
+export default Logo;
